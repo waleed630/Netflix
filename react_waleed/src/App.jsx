25b6b482879b448bb940e071295f66cf0bb776f5
Waleed <m.waleedbinlatif@gmail.com>
2025-03-18 08:28:37 +0500

    EventPropagation

--- react_waleed/src/App.jsx
+++ react_waleed/src/App.jsx
@@ -1,16 +1,16 @@
 import NetflixSeries from "./components/NetflixSeries";
 //import "./components/Netflix.css";
 import "./components/Netflix.module.css";
-import { EventHandling } from "./components/EventHandling";
-import { EventProps } from "./components/EventProps";
+// import { EventHandling } from "./components/EventHandling";
+// import { EventProps } from "./components/EventProps";
 
 export const App = () => {
   return (
     <section className="container">
-      {/* <h1 className="card-heading">List of Best Netflix Series</h1>
-      <NetflixSeries />; */}
+      <h1 className="card-heading">List of Best Netflix Series</h1>
+      <NetflixSeries />;
       {/* <EventHandling/> */}
-      <EventProps />
+      {/* <EventProps /> */}
     </section>
   );
 };
--- react_waleed/src/components/EventProps.jsx
+++ react_waleed/src/components/EventProps.jsx
@@ -16,15 +16,17 @@ export const EventProps = () => {
 };
 
 const WelcomeUser = (props) => {
-    const{onClick,onMouseEnter}=props; //if we dont write this line (destructuring)
+  const { onClick, onMouseEnter } = props; //if we dont write this line (destructuring)
   const handleGreeting = () => {
     console.log(`Hey user Welcome`);
     props.onClick();
   };
   return (
     <>
-      <button onClick={onClick}>Click</button>  {/*than here we ll write props.onClick*/}
-      <button onMouseEnter={onMouseEnter}>Hover me</button> {/*and props.onMouseEnter*/}
+      <button onClick={onClick}>Click</button>{" "}
+      {/*than here we ll write props.onClick*/}
+      <button onMouseEnter={onMouseEnter}>Hover me</button>{" "}
+      {/*and props.onMouseEnter*/}
       <button onClick={handleGreeting}>Greetings</button>
     </>
   );
